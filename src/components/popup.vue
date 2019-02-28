b6f458b30081ffa753cbbcf47be66a7cb60ff6c5
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2019-02-28 23:41:00 +0100

    close popup after opening wallet

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -3,9 +3,15 @@
     Hello firefox extension. I am Vue!
 
     <br>
-    <a href="wallet.html" target="_blank">open wallet</a>
+    <a href="wallet.html" target="_blank" v-on:click="closePopup">open wallet</a>
   </div>
 </template>
 <script>
-export default { };
+export default {
+  methods: {
+    closePopup: (event) => {
+      setTimeout(() => window.close(), 50);
+    }
+  }
+ };
 </script>
