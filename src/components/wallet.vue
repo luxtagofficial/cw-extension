d1573638cc1f3065f78d22ba1ac437e92676a8b1
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2019-03-10 00:06:57 +0100

    menu

--- src/components/wallet.vue
+++ src/components/wallet.vue
@@ -17,7 +17,7 @@
               <v-divider></v-divider>
 
               <v-list dense class="pt-0">
-                <v-list-tile @click  to="/">
+                <v-list-tile @click to="/">
                   <v-list-tile-action>
                     <v-icon>home</v-icon>
                   </v-list-tile-action>
@@ -26,20 +26,63 @@
                     <v-list-tile-title>Home</v-list-tile-title>
                   </v-list-tile-content>
                 </v-list-tile>
-                <v-list-tile @click to="/create-wallet">
+
+                <v-list-tile @click>
                   <v-list-tile-action>
-                    <v-icon>payment</v-icon>
+                    <v-icon>account_circle</v-icon>
                   </v-list-tile-action>
 
                   <v-list-tile-content>
-                    <v-list-tile-title>Wallet </v-list-tile-title>
+                    <v-list-tile-title>Account</v-list-tile-title>
                   </v-list-tile-content>
                 </v-list-tile>
+
+                <v-list-group no-action prepend-icon="dns">
+                  <template v-slot:activator>
+                    <v-list-tile>
+                      <v-list-tile-title>Namespaces</v-list-tile-title>
+                    </v-list-tile>
+                  </template>
+                  <v-list-tile @click>
+                    <v-list-tile-title>Management</v-list-tile-title>
+                    <v-list-tile-action>
+                      <v-icon>add</v-icon>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </v-list-group>
+
+                <v-list-group no-action prepend-icon="group_work">
+                  <template v-slot:activator>
+                    <v-list-tile>
+                      <v-list-tile-title>Assets</v-list-tile-title>
+                    </v-list-tile>
+                  </template>
+                  <v-list-tile @click>
+                    <v-list-tile-title>Management</v-list-tile-title>
+                    <v-list-tile-action>
+                      <v-icon>add</v-icon>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </v-list-group>
+
+                <v-list-group no-action prepend-icon="payment">
+                  <template v-slot:activator>
+                    <v-list-tile>
+                      <v-list-tile-title>Wallet</v-list-tile-title>
+                    </v-list-tile>
+                  </template>
+                  <v-list-tile @click to="/create-wallet">
+                    <v-list-tile-title>Generate Wallet</v-list-tile-title>
+                    <v-list-tile-action>
+                      <v-icon>add</v-icon>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </v-list-group>
               </v-list>
             </v-navigation-drawer>
           </v-flex>
           <v-flex xs10 px-4>
-             <router-view></router-view>
+            <router-view></router-view>
           </v-flex>
         </v-layout>
       </v-container>
