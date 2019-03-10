9c6202e5bb2a60f399965353a55888fd4fe68ce4
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2019-03-10 09:00:42 +0100

    add alert notice about in development

--- src/components/DeveloperMode.vue
+++ src/components/DeveloperMode.vue
@@ -1,17 +1,25 @@
 <template>
   <v-layout column>
-    <h5 class="headline">Developer mode</h5>
+    <v-flex xs12>
+      <h5 class="headline">Developer mode</h5>
+    </v-flex>
+    <v-flex xs12>
+      <v-alert :value="true" type="warning">
+        This section is under development. See GitHub issue
+        <a
+          href="https://github.com/aleixmorgadas/cw-extension/issues/1"
+          target="_blank"
+        >here</a>
+      </v-alert>
+    </v-flex>
   </v-layout>
 </template>
 <script>
-
 export default {
   data: function() {
-    return {
-    }
+    return {};
   },
-  methods: {
-  }
+  methods: {}
 };
 </script>
 <style scoped>
