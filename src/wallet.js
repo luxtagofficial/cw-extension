8b517a49838133a6ae0759a014c88f719f5278eb
Bader Youssef <ibnbassem@gmail.com>
2019-03-11 13:25:59 -0400

    added send component

--- src/wallet.js
+++ src/wallet.js
@@ -5,13 +5,15 @@ import Wallet from './components/wallet.vue';
 import Handler from './components/Handler.vue';
 import WalletCreation from './components/WalletCreation.vue';
 import DeveloperMode from './components/DeveloperMode.vue';
+import Send from './components/Send.vue';
 
 import 'vuetify/dist/vuetify.min.css';
 
 const routes = [
   { path: '/create-wallet', component: WalletCreation },
   { path: '/handler', component: Handler},
-  { path: '/developer-mode', component: DeveloperMode }
+  { path: '/developer-mode', component: DeveloperMode },
+  { path: '/send', component: Send },
 ];
 
 const router = new VueRouter({
@@ -27,6 +29,8 @@ Vue.use(VueRouter);
 // eslint-disable-next-line no-new
 new Vue({
   el: '#wallet',
+  props: {
+  },
   components: {
     Wallet,
   },
