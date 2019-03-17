6d4e290690973f5c0bbaba7b6365c26643e26637
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2019-03-17 01:20:54 +0100

    storing wallets on localStorage

--- src/infrastructure/StateRepository.js
+++ src/infrastructure/StateRepository.js
@@ -1,8 +1,26 @@
+import { Account, NetworkType } from "nem2-sdk";
+
 /* eslint-disable class-methods-use-this */
+
+// Ports & Adapters
+
+const walletsToJSON = wallets => JSON.stringify(wallets.map(wallet => ({
+  name: wallet.name,
+  privateKey: wallet.account.privateKey,
+  node: wallet.node,
+})));
+
+const jsonToWallets = json => JSON.parse(json).map(wallet => ({
+  name: wallet.name,
+  account: Account.createFromPrivateKey(wallet.privateKey, NetworkType.MIJIN_TEST),
+  node: wallet.node,
+}));
+
 class StateRepository {
   constructor() {
+    const wallets = localStorage.getItem('wallets');
     this.state = {
-      wallets: [],
+      wallets: wallets != null ? jsonToWallets(wallets) : [],
     };
   }
 
@@ -12,6 +30,16 @@ class StateRepository {
 
   storeWallet(walletName, account, node) {
     this.state.wallets.push({ name: walletName, account, node });
+    localStorage.setItem('wallets', walletsToJSON(this.state.wallets));
+  }
+
+  removeWallet(wallet) {
+    let walletIndex;
+    this.state.wallets.forEach((value, index) => {
+      if (value.name === wallet.name) walletIndex = index;
+    });
+    if (walletIndex !== undefined) this.state.wallets.splice(walletIndex, 1);
+    localStorage.setItem('wallets', walletsToJSON(this.state.wallets));
   }
 }
 
