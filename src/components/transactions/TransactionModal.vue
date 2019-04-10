e89575cb076443cbeefb358fcb15eb6cba066e7b
decentraliser <jeanluc.strullu@gmail.com>
2019-04-10 14:37:08 +0800

    basic tx detail modal

---
+++ src/components/transactions/TransactionModal.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-dialog
+    v-model="modal"
+    max-width="80%"
+  >
+    <v-layout>
+      <v-flex
+        xs12
+      >
+        <v-card>
+          <v-card-title primary-title>
+            <div class="monospaced">
+              <h3 class="headline mb-0">
+                {{ tx.type }}
+              </h3>
+
+              <span class="clearfix bold">
+                ID:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.id }}
+              </span>
+
+              <span class="clearfix bold">
+                transactionHash:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.transactionHash }}
+              </span>
+
+              <span class="clearfix bold">
+                signer:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.signer }}
+              </span>
+
+              <span class="clearfix bold">
+                recipient:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.recipient }}
+              </span>
+
+              <span class="clearfix bold">
+                amount:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.amount.toLocaleString()  }}
+              </span>
+
+              <span class="clearfix bold">
+                assetId:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.assetId }}
+              </span>
+
+              <span class="clearfix bold">
+                fee:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.fee }}
+              </span>
+
+              <span class="clearfix bold">
+                blockNumber:
+              </span>
+              <span class="clearfix  mb-2">
+                {{ tx.blockNumber.toLocaleString() }}
+              </span>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'TransactionModal',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    tx: {
+      type: Object,
+    },
+    modal: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+  },
+  watch: {
+    isShow: {
+      handler(newVal) {
+        this.modal = newVal;
+      },
+    },
+    modal: {
+      handler(newVal) {
+        if (newVal === false) {
+          this.$emit('close');
+        }
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/transactions/Transactions.vue
+++ src/components/transactions/Transactions.vue
@@ -4,93 +4,112 @@
     xs12
   >
     <v-layout
-      row
-      mb-4
-      mt-4
+      column
+      xs12
     >
       <v-layout
         row
-        fill-height
-        justify-start
-        pl-3
-        xs3
+        mb-4
+        mt-4
       >
-        <h5 class="headline pt-3">
-          Transactions
-        </h5>
+        <v-layout
+          row
+          fill-height
+          justify-start
+          pl-3
+          xs3
+        >
+          <h5 class="headline pt-3">
+            Transactions
+          </h5>
+        </v-layout>
+        <v-layout
+          row
+          fill-height
+          justify-end
+          xs9
+        >
+          <v-btn
+            color="primary mx-0"
+            @click="refresh"
+          >
+            Refresh
+          </v-btn>
+          <v-btn
+            class="ml-3"
+            color="primary mx-0"
+            @click="loadMore"
+          >
+            Load more
+          </v-btn>
+        </v-layout>
       </v-layout>
-      <v-layout
-        row
+      <v-container
+        fluid
         fill-height
-        justify-end
-        xs9
       >
-        <v-btn
-          color="primary mx-0"
-          @click="refresh"
-        >
-          Refresh
-        </v-btn>
-        <v-btn
-          class="ml-3"
-          color="primary mx-0"
-          @click="loadMore"
-        >
-          Load more
-        </v-btn>
-      </v-layout>
+        <v-layout child-flex>
+          <v-data-table
+            :headers="headers"
+            :items="transactions"
+            disable-initial-sort
+            :rows-per-page-items="rowsPerPageOptions"
+          >
+            <template v-slot:items="props">
+              <tr
+                class="pointer"
+                @click="showModal(props.item.id)"
+              >
+                <td class="text-xs-right">
+                  <span class="clearfix">
+                    <pre>{{ props.item.time }}</pre>
+                  </span>
+                  <span class="clearfix">
+                    <pre>{{ props.item.blockNumber }}</pre>
+                  </span>
+                </td>
+                <td class="text-xs-right">
+                  <span class="clearfix">
+                    <pre>{{ props.item.signer }}</pre>
+                  </span>
+                  <span class="clearfix">
+                    <pre>{{ props.item.recipient }}</pre>
+                  </span>
+                </td>
+                <td class="text-xs-right">
+                  <span class="clearfix">
+                    <pre>{{ props.item.amount }} ({{ props.item.fee }})</pre>
+                  </span>
+                  <span class="clearfix">
+                    <pre>{{ props.item.assetId }}</pre>
+                  </span>
+                </td>
+                <td class="text-xs-right">
+                  {{ props.item.type }}
+                </td>
+              </tr>
+            </template>
+          </v-data-table>
+        </v-layout>
+      </v-container>
     </v-layout>
-    <v-container
-      fluid
-      fill-height
-    >
-      <v-layout child-flex>
-        <v-data-table
-          :headers="headers"
-          :items="transactions"
-          disable-initial-sort
-          :rows-per-page-items="rowsPerPageOptions"
-        >
-          <template v-slot:items="props">
-            <td class="text-xs-right">
-              <span class="clearfix">
-                <pre>{{ props.item.time }}</pre>
-              </span>
-              <span class="clearfix">
-                <pre>{{ props.item.blockNumber }}</pre>
-              </span>
-            </td>
-            <td class="text-xs-right">
-              <span class="clearfix">
-                <pre>{{ props.item.signer }}</pre>
-              </span>
-              <span class="clearfix">
-                <pre>{{ props.item.recipient }}</pre>
-              </span>
-            </td>
-            <td class="text-xs-right">
-              <span class="clearfix">
-                <pre>{{ props.item.amount }} ({{ props.item.fee }})</pre>
-              </span>
-              <span class="clearfix">
-                <pre>{{ props.item.assetId }}</pre>
-              </span>
-            </td>
-            <td class="text-xs-right">
-              {{ props.item.type }}
-            </td>
-          </template>
-        </v-data-table>
-      </v-layout>
-    </v-container>
+
+    <TransactionModal
+      :modal="modal"
+      :tx="activeTransaction"
+      @close="modalClosed"
+    />
   </v-layout>
 </template>
 <script>
 import StateRepository from '../../infrastructure/StateRepository';
+import TransactionModal from './TransactionModal.vue';
 
 export default {
   name: 'Transactions',
-  components: {},
+  components: {
+    TransactionModal,
+  },
   props: {
     transactions: {
       default() {
@@ -111,6 +130,8 @@ export default {
       rowsPerPageOptions: [
         25, 50, { text: 'All', value: -1 },
       ],
+      activeTransaction: this.transactions[0],
+      modal: false,
     };
   },
   methods: {
@@ -119,6 +140,13 @@ export default {
     },
     loadMore() {
       StateRepository.getAccountTransactionsById('more');
+    },
+    showModal(clickedTx) {
+      this.activeTransaction = this.transactions.find(t => t.id === clickedTx);
+      this.modal = true;
+    },
+    modalClosed() {
+      this.modal = false;
     },
   },
 };
