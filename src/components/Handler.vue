2fcd2e3782ded9588fd0c470df25fd14af394e02
decentraliser <jeanluc.strullu@gmail.com>
2019-04-12 17:51:13 +0800

    merge

--- src/components/Handler.vue
+++ src/components/Handler.vue
@@ -1,19 +1,91 @@
 <template>
   <v-layout column>
-    <h5 class="headline">Transaction handled</h5>
-    <p>{{transaction}}</p>
+    <h5 class="headline">Transaction Handler</h5>
+      <v-flex xs12>
+        <v-card>
+          
+          <v-card-title>
+            <div>
+              <div class="monospaced-bold clearfix mb-2">
+                Recipient: {{ transactions[0].recipient.pretty() }}
+              </div>
+              
+              <div class="monospaced clearfix mb-1">
+                <ul v-for="mosaic in transactions[0].mosaics" :key="mosaic.amount.compact()">
+                  <li> 
+                    {{ mosaic.amount.compact() }}
+                  </li>
+                </ul>
+              </div>
+
+               <div class="monospaced clearfix mb-1">
+                Message: {{ transactions[0].message.payload }}
+              </div>
+
+               <div class="monospaced clearfix mb-1">
+                Chain Destination: {{ txURL.chainId }}
+              </div>
+
+              <div class="monospaced clearfix mb-1">
+                Endpoint: {{ txURL.endpoint }}
+              </div>
+              </div>
+          </v-card-title>
+
+          <v-card-actions>
+
+            <v-btn
+          color="primary mx-0"
+          @click="toggleDialog = true"
+          >Accept transaction</v-btn>
+
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+   <v-flex xs12>
+      <v-alert :value="true" type="info">
+        Please confirm the details of the transaction before proceeding.
+      </v-alert>
+    </v-flex>
+    <Confirmation :value="toggleDialog" :transactions="transactions" :title="title" :body="body" :maxWidth="500"  />
   </v-layout>
 </template>
 <script>
+
+import { TransactionURI, URIFormat } from 'nem2-uri-scheme';
+import { TransferTransaction, Deadline, Address, NetworkCurrencyMosaic, NetworkType, PlainMessage, TransactionType } from 'nem2-sdk';
+import Confirmation from './Confirmation.vue';
+
 export default {
   data: function () {
-    return { transaction: {} }
+    return { 
+      transactions: [],
+      txURL: Object,
+      toggleDialog: false,
+      title: "Are sure you want to accept this transaction?",
+      body: "This transaction came from a URI link, and is to be sent to address as specified by the URI"
+
+    }
   },
+
+  components: {
+    Confirmation
+  },
+
   created: function () {
     const transactionQuery = this.$route.query.transaction;
-    this.transaction = JSON.parse(this.$route.query.transaction.substring(
-      'web+nem://transaction?data='.length, transactionQuery.length));
-  }
+
+    const txDetail = 
+    (transactionQuery.substring('web+nem://transaction?data='.length, transactionQuery.length))
+    .split('?')
+    .map((detail) => detail.substring(detail.indexOf('=')+1, detail.length));
+
+   const transactionURI = new TransactionURI(URIFormat.serialized, txDetail[1], txDetail[2], txDetail[3]);
+
+   const uriTX = transactionURI.toTransaction();
+   this.transactions.push(uriTX);
+   this.txURL = transactionURI;
+  },
 };
 </script>
 <style scoped>
