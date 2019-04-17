f14d37d51a7c1cf5ea5894a92104b9d21b4e1df4
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2019-04-17 13:46:12 +0200

    license header

--- src/components/WalletSelector.vue
+++ src/components/WalletSelector.vue
@@ -1,3 +1,20 @@
+// Copyright (C) 2019 Contributors as noted in the AUTHORS file
+// 
+// This file is part of nem2-wallet-browserextension.
+// 
+// nem2-wallet-browserextension is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// nem2-wallet-browserextension is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with nem2-wallet-browserextension.  If not, see <http://www.gnu.org/licenses/>.
+
 <template>
   <span>
     <v-select
